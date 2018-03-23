4370edc19c01293a84f040b6aab1ed241da745a8
Richard Cavell <richardcavell@mail.com>
2018-03-23 15:43:46 +0000

    I am providing an alternative version of the software, which uses getchar() instead of fread(), and putchar() instead of fwrite().
    Signed-off-by: Richard Cavell <richardcavell@mail.com>

---
+++ alternative/README.txt
@@ -0,0 +1,12 @@
+reverse-c (alternative version)
+A program by Richard Cavell
+https://github.com/richardcavell/reverse-c
+(c) 2017-2018
+
+This alternative version of reverse uses getchar() and putchar() instead of
+fread() and fwrite().
+
+The main version will probably run faster. However, you are welcome to study
+this one as well, which I provide for that purpose.
+
+Richard Cavell
---
+++ alternative/reverse.c
@@ -0,0 +1,66 @@
+/* reverse.c (alternative version)
+ *
+ * A program by Richard Cavell
+ * (c) 2017-2018
+ *
+ * https://github.com/richardcavell
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF 100
+
+struct block
+{
+    struct block *prev;
+    char text[BUF];
+};
+
+int main(void)
+{
+    int c;
+    struct block *p = NULL;
+    char *t = NULL;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (p == NULL || t == p->text)
+        {
+            struct block *q = malloc(sizeof *q);
+
+            if (q == NULL)
+            {
+                fprintf(stderr, "Error: Out of memory\n");
+
+                while (p)
+                    { q = p->prev; free(p); p = q; }
+
+                exit(EXIT_FAILURE);
+            }
+
+            q->prev = p;
+            p = q;
+            t = &p->text[BUF];
+        }
+
+        *--t = c;
+    }
+
+    while (p)
+    {
+        struct block *q = p->prev;
+
+        while (t < &p->text[BUF])
+            putchar(*t++);
+
+        free(p);
+
+        p = q;
+
+        if (p)
+            t = p->text;
+    }
+
+    return EXIT_SUCCESS;
+}
